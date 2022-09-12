51101707e348da1fff7679d6b32f88eeb086a8d4
Martin POUTRAIN <martin.poutrain@student.junia.com>
2022-09-12 08:12:59 +0200

    Ajoutez des fichiers projet.

---
+++ TP1.cpp
@@ -0,0 +1,11 @@
+﻿// TP1.cpp : définit le point d'entrée de l'application.
+//
+
+/* Premier programme en C */
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+	printf("Hello World\n");
+	return EXIT_SUCCESS;
+}
